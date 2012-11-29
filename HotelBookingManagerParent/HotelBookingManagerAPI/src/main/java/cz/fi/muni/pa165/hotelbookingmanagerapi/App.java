c19c1f09b62f57c37a1c1b1df6a174e6dffa5794
marian-r <Grizly21@gmail.com>
2012-11-29 15:14:11 +0000

    Revert to previous version

---
+++ HotelBookingManagerParent/HotelBookingManagerAPI/src/main/java/cz/fi/muni/pa165/hotelbookingmanagerapi/App.java
@@ -0,0 +1,13 @@
+package cz.fi.muni.pa165.hotelbookingmanagerapi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
