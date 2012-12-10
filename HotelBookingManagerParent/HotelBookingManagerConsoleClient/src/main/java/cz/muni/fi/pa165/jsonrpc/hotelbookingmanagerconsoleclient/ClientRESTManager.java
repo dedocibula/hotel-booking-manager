8a595232d7c52ec41e21317bb413e2b4fc570ad5
marian-r <Grizly21@gmail.com>
2012-12-10 00:23:36 +0000

    Added returning error codes in case of error to Client REST service.

--- HotelBookingManagerParent/HotelBookingManagerConsoleClient/src/main/java/cz/muni/fi/pa165/jsonrpc/hotelbookingmanagerconsoleclient/ClientRESTManager.java
+++ HotelBookingManagerParent/HotelBookingManagerConsoleClient/src/main/java/cz/muni/fi/pa165/jsonrpc/hotelbookingmanagerconsoleclient/ClientRESTManager.java
@@ -33,13 +33,38 @@ public class ClientRESTManager {
         webResource = client.resource(url);
     }
 
-    public ClientTO findClient(Long id) {
+    /**
+	 * Returns client with given id.
+	 * Error status codes are:
+	 * 400 Bad request				Not applicable for now, cannot send null id to server, because IllegalArgumentException is thrown.
+	 * 404 Not found				If client with given id was not found.
+	 * 500 Internal server error	Other error.
+	 * 
+	 * @param id
+	 * @return client with given id.
+	 * @throws IllegalArgumentException if id is null.
+	 * @throws ClientHandlerException if signals a failure to process the HTTP request or HTTP response.
+	 * @throws Class UniformInterfaceException when the status code of the HTTP response indicates a response that is not expected.
+	 */
+	public ClientTO findClient(Long id) {
+		if (id == null) {
+			throw new IllegalArgumentException("Id cannot be null.");
+		}
         return webResource.path("findClient")
                           .queryParam("id", id.toString())
                           .accept(MediaType.APPLICATION_JSON)
                           .get(ClientTO.class);
     }
     
+	/**
+	 * Returns all clients.
+	 * Error status codes are:
+	 * 500 Internal server error	In case of any error on service layer.
+	 * 
+	 * @return all clients.
+	 * @throws ClientHandlerException if signals a failure to process the HTTP request or HTTP response.
+	 * @throws Class UniformInterfaceException when the status code of the HTTP response indicates a response that is not expected.
+	 */
     public List<ClientTO> findAllClients() {
         List<ClientTO> clients = new ArrayList<>();
         String json = webResource.path("findAllClients")
@@ -52,6 +77,18 @@ public class ClientRESTManager {
         return clients;
     }
     
+	/**
+	 * Returns client with given name.
+	 * Error status codes are:
+	 * 400 Bad request				If name is null or empty string.
+	 * 404 Not found				If client with given name was not found.
+	 * 500 Internal server error	Other error.
+	 * 
+	 * @param name
+	 * @return client with given name.
+	 * @throws ClientHandlerException if signals a failure to process the HTTP request or HTTP response.
+	 * @throws Class UniformInterfaceException when the status code of the HTTP response indicates a response that is not expected.
+	 */
     public List<ClientTO> findClientsByName(String name) {
         List<ClientTO> clients = new ArrayList<>();
         String json = webResource.path("findClientsByName")
@@ -65,6 +102,17 @@ public class ClientRESTManager {
         return clients;
     }
     
+	/**
+	 * Creates client.
+	 * Error status codes are:
+	 * 400 Bad request				In case of invalid client.
+	 * 500 Internal server error	Other error.
+	 * 
+	 * @param client
+	 * @return response from server.
+	 * @throws ClientHandlerException if signals a failure to process the HTTP request or HTTP response.
+	 * @throws Class UniformInterfaceException when the status code of the HTTP response indicates a response that is not expected.
+	 */
     public ClientResponse createClient(ClientTO client) {
         return webResource.path("createClient")
                 .type(MediaType.APPLICATION_JSON)
@@ -72,6 +120,17 @@ public class ClientRESTManager {
                 .post(ClientResponse.class, client);
     }
     
+	/**
+	 * Updates client.
+	 * Error status codes are:
+	 * 400 Bad request				In case of invalid client.
+	 * 500 Internal server error	Other error.
+	 * 
+	 * @param client
+	 * @return response from server.
+	 * @throws ClientHandlerException if signals a failure to process the HTTP request or HTTP response.
+	 * @throws Class UniformInterfaceException when the status code of the HTTP response indicates a response that is not expected.
+	 */
     public ClientResponse updateClient(ClientTO client) {
         return webResource.path("updateClient")
                 .type(MediaType.APPLICATION_JSON)
@@ -79,7 +138,27 @@ public class ClientRESTManager {
                 .post(ClientResponse.class, client);
     }
     
+	/**
+	 * Deletes client.
+	 * Error status codes are:
+	 * 400 Bad request				If client is invalid.
+	 * 404 Not found				If client was not found.
+	 * 417 Expectation Failed		In case of DB error, mainly DB constraint.
+	 * 500 Internal server error	Other error.
+	 * 
+	 * @param client
+	 * @return response from server.
+	 * @throws IllegalArgumentException if client is null or has null id.
+	 * @throws ClientHandlerException if signals a failure to process the HTTP request or HTTP response.
+	 * @throws Class UniformInterfaceException when the status code of the HTTP response indicates a response that is not expected.
+	 */
     public ClientResponse deleteClient(ClientTO client) {
+		if (client == null) {
+			throw new IllegalArgumentException("Client cannot be null.");
+		}
+		if (client.getId() == null) {
+			throw new IllegalArgumentException("Client id cannot be null.");
+		}
         return webResource.path("deleteClient")
                 .queryParam("id", client.getId().toString())
                 .type(MediaType.APPLICATION_JSON)
--- HotelBookingManagerParent/HotelBookingManagerWeb/src/main/java/cz/fi/muni/pa165/hotelbookingmanagerrest/ClientRESTService.java
+++ HotelBookingManagerParent/HotelBookingManagerWeb/src/main/java/cz/fi/muni/pa165/hotelbookingmanagerrest/ClientRESTService.java
@@ -1,6 +1,5 @@
 package cz.fi.muni.pa165.hotelbookingmanagerrest;
 
-import com.sun.jersey.api.Responses;
 import cz.fi.muni.pa165.hotelbookingmanagerapi.service.ClientService;
 import cz.fi.muni.pa165.hotelbookingmanagerapi.transferobjects.ClientTO;
 import java.util.List;
@@ -8,6 +7,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,68 +17,126 @@ import org.springframework.stereotype.Component;
 @Component
 @Path("/clients")
 public class ClientRESTService {
-    
-    @Autowired
-    private ClientService clientService;
-    
-    @GET
-    @Path("/findClient")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ClientTO getClientInJSON(@QueryParam("id") Long id) {
-        ClientTO client = clientService.findClient(id);
-        if (client == null)
-            throw new WebApplicationException(
-                    Response.status(Responses.NOT_FOUND)
-                            .entity("Clients with id " + id + " weren't found")
-                            .build());
-        return client;
-    }
-    
-    @GET
-    @Path("/findAllClients")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<ClientTO> getAllClientsInJSON() {
-        return clientService.findAllClients();
-    }
-    
-    @GET
-    @Path("/findClientsByName")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<ClientTO> getCliensByNameInJSON(@QueryParam("name") String name) {
-        List<ClientTO> temp = clientService.findClientsByName(name);
-        if (temp.isEmpty())
-            throw new WebApplicationException(
-                    Response.status(Responses.NOT_FOUND)
-                            .entity("Clients with name " + name + " weren't found")
-                            .build());
-        return temp;
-    }
 
-    @POST
-    @Path("/createClient")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response createClient(ClientTO client) {
-        clientService.createClient(client);
-        String result = "Client saved : " + client;
-        return Response.ok(result).build();
-    }
-    
-    @POST
-    @Path("/updateClient")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response updateClient(ClientTO client) {
-        clientService.updateClient(client);
-        String result = "Client updated : " + client;
-        return Response.ok(result).build();
-    }
-    
-    @DELETE
-    @Path("/deleteClient")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response deleteClient(@QueryParam("id") Long id) {
-        ClientTO client = clientService.findClient(id);
-        clientService.deleteClient(client);
-        String result = "Client removed : " + client;
-        return Response.ok(result).build();
-    }
+	@Autowired
+	private ClientService clientService;
+
+	@GET
+	@Path("/findClient")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ClientTO getClientInJSON(@QueryParam("id") Long id) {
+		ClientTO client;
+		
+		try {
+			client = clientService.findClient(id);
+		} catch (IllegalArgumentException ex) {
+			// 400
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
+		
+		if (client == null) {
+			// 404
+			throw new WebApplicationException(
+					Response.status(Response.Status.NOT_FOUND)
+					.entity("Clients with id " + id + " weren't found")
+					.build());
+		}
+		return client;
+	}
+
+	@GET
+	@Path("/findAllClients")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<ClientTO> getAllClientsInJSON() {
+		return clientService.findAllClients();
+	}
+
+	@GET
+	@Path("/findClientsByName")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<ClientTO> getCliensByNameInJSON(@QueryParam("name") String name) {
+		List<ClientTO> temp;
+		
+		try {
+			temp = clientService.findClientsByName(name);
+		} catch (IllegalArgumentException ex) {
+			// 400
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
+		
+		if (temp.isEmpty()) {
+			// 404
+			throw new WebApplicationException(
+					Response.status(Response.Status.NOT_FOUND)
+					.entity("Clients with name " + name + " weren't found")
+					.build());
+		}
+		return temp;
+	}
+
+	@POST
+	@Path("/createClient")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createClient(ClientTO client) {
+		try {
+			clientService.createClient(client);
+		} catch (IllegalArgumentException ex) {
+			// 400
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
+		
+		String result = "Client saved : " + client;
+		// 200
+		return Response.ok(result).build();
+	}
+
+	@POST
+	@Path("/updateClient")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateClient(ClientTO client) {
+		try {
+			clientService.updateClient(client);
+		} catch (IllegalArgumentException ex) {
+			// 400
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
+		
+		String result = "Client updated : " + client;
+		// 200
+		return Response.ok(result).build();
+	}
+
+	@DELETE
+	@Path("/deleteClient")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response deleteClient(@QueryParam("id") Long id) {
+		ClientTO client;
+		
+		try {
+			client = clientService.findClient(id);
+		} catch (IllegalArgumentException ex) {
+			// 400
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
+		
+		if (client == null) {
+			// 404
+			throw new WebApplicationException(Response.Status.NOT_FOUND);
+		}
+		
+		try {
+			clientService.deleteClient(client);
+		} catch (IllegalArgumentException ex) {
+			// 400
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		} catch (DataAccessException ex) {
+			// Mainly for DB constraint
+			// 417
+			throw new WebApplicationException(417);
+		}
+		
+		String result = "Client removed : " + client;
+		// 200
+		return Response.ok(result).build();
+	}
 }
